17e6ecef1fcbdb09e1e54964097541bed5cb2958
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-14 20:19:13 +0200

    Update I-good.py

--- I-good.py
+++ I-good.py
@@ -2,35 +2,40 @@
 
 from abc import ABC, abstractmethod
 
-class Bird(ABC):
-    @abstractmethod
-    def fly(self):
-        pass
 
+class Walkable(ABC):
     @abstractmethod
     def walk(self):
         pass
 
-class Ostriche(Bird):
-    def fly(self):
-        raise Exception("Ostriche is not flying")
 
+class Flyable(ABC):
+    @abstractmethod
+    def fly(self):
+        pass
+
+
+class Ostriche(Walkable):
     def walk(self):
         print("Ostriche is walking")
 
-class Eagle(Bird):
+
+class Eagle(Walkable, Flyable):
     def fly(self):
         print("Eagle is flying")
 
     def walk(self):
         print("Eagle is walking")
 
+
 try:
     obj = Eagle()
     obj.fly()
     obj.walk()
+
     obj2 = Ostriche()
     obj2.walk()
-    obj2.fly()
+
 except Exception as e:
     print(e)
+
